9e2eacb3599d771dfcb80243682b9ecc581823c5
Антон Деревягин <antonderevagin@MacBook-Air-Anton.local>
2022-12-12 17:01:22 +0300

    zadacha1

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+// Не использовать Math.Pow() и аналоги!
+
+int MethodRead(string messege)
+{
+    Console.WriteLine(messege);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void MethodDegree(int a, int b)
+{
+    int res = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        res = res * a;
+        
+    }
+    Console.WriteLine(res);
+
+}
+
+int numberA = MethodRead("Введите число А: ");
+int numberB = MethodRead("Введите число B: ");
+MethodDegree(numberA, numberB);
+
+
