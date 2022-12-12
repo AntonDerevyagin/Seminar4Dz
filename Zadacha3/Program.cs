bb04387e84c122a643bf445c2f73187c173e4a1d
Антон Деревягин <antonderevagin@MacBook-Air-Anton.local>
2022-12-12 18:03:14 +0300

    Zadacha3-1

--- Zadacha3/Program.cs
+++ Zadacha3/Program.cs
@@ -1,37 +1,6 @@
 ﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. 
 // Данные вводятся с консоли пользователем
 
-// int MethodRead(string messege)
-// {
-//     Console.WriteLine(messege);
-//     return Convert.ToInt32(Console.ReadLine());
-// }
-
-// //ЗАДАЧА НА МАССИВ
-// int[] array = new int [15];
-// //int number = new Random().Next(0,2);
-// for (int i = 0; i < array.Length; i++)// length  длина массива
-// {
-//     array[i] = new Random().Next(0,2); // это мы рандомим каждый последующий элемент в массиве
-// }
-// Console.WriteLine(string.Join(",", array)); // Джоин это вывод массива в строку 
-
-
-// int [] array = new int[8];
-// {
-    
-//     for (int i = 0; i <= array.Length; i++)
-//     {
-//         array[i] = new Random().Next(1,10);
-        
-//     }
-//     Console.WriteLine(string.Join(",", array[]));
-    
-   
-// }
-
-// int lenArray = MethodRead("Введите длину массива: ");
-
 int len = ReadInt("Введите длинну массива: ");
 
 int[] array = new int[len];
